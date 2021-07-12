110481df0b9e76420873ce54edead83056d74085
Diego Adilson Vieira <diegoavieira@gmail.com>
2021-07-12 00:10:00 -0300

    build: configuration

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartIndex';
+import { productsMount } from 'products/ProductsIndex';
+import { cartMount } from 'cart/CartIndex';
 
 console.log('container');
+
+productsMount(document.querySelector('#my-products'));
+cartMount(document.querySelector('#my-cart'));
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,22 @@
 import faker from 'faker';
 
-let products = '';
+const productsMount = (el) => {
+    let products = '';
+    
+    for (let i = 0; i < 5; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`;
+    }
+    
+    el.innerHTML = products;
+};
 
-for (let i = 0; i < 5; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-products');
+
+    if (el) {
+        productsMount(el);
+    }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+export { productsMount };
